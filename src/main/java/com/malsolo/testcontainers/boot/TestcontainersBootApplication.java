222fa308b57843eee9a6e2df8e72d22ad346682d
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-02-03 07:37:39 +0100

    Testcontainers-boot: project from start.spring.io.

---
+++ src/main/java/com/malsolo/testcontainers/boot/TestcontainersBootApplication.java
@@ -0,0 +1,13 @@
+package com.malsolo.testcontainers.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malsolo/testcontainers/boot/TestcontainersBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malsolo.testcontainers.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
