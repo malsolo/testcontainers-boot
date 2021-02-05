c255dc094f4324a31b3a4bd0cb9c99f2bab93afe
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-02-05 10:11:00 +0100

    Testcontainers-boot: kafka test with testcontainers.

--- src/main/java/com/malsolo/testcontainers/boot/kafka/KafkaBootProducer.java
+++ src/main/java/com/malsolo/testcontainers/boot/kafka/KafkaBootProducer.java
@@ -2,7 +2,10 @@ package com.malsolo.testcontainers.boot.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,7 +19,18 @@ public class KafkaBootProducer {
     public void sendMessage(Integer key, String message) {
         var topic = kafkaProperties.getProducer().getTopic();
         log.info("About to send a message {} with key {} to topic {}", message, key, topic);
-        kafkaTemplate.send(topic, key, message);
+        var future = kafkaTemplate.send(topic, key, message);
+        future.addCallback(new KafkaSendCallback<>() {
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                log.info("Successfully sent message {} with key {} to topic {}", message, key, topic);
+            }
+
+            @Override
+            public void onFailure(KafkaProducerException ex) {
+                log.error("Error sending message {} with key {} to topic {}: {}", message, key, topic, ex.getMessage());
+            }
+        });
     }
 
 }
--- src/test/java/com/malsolo/testcontainers/boot/kafka/KafkaTest.java
+++ src/test/java/com/malsolo/testcontainers/boot/kafka/KafkaTest.java
@@ -4,20 +4,46 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.cassandra.CassandraReactiveDataAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude={CassandraReactiveDataAutoConfiguration.class})
+@EnableAutoConfiguration(exclude={CassandraReactiveDataAutoConfiguration.class,
+    CassandraAutoConfiguration.class, CassandraDataAutoConfiguration.class})
+@Testcontainers
+@Slf4j
 public class KafkaTest {
+
+    public static final String KAFKA_IMAGE_NAME = "confluentinc/cp-kafka:6.0.1";
+
+    @Container
+    public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(KAFKA_IMAGE_NAME));
 
     @Autowired
     private KafkaConsumerWithQueue consumer;
     @Autowired
     private KafkaBootProducer producer;
+
+    @DynamicPropertySource
+    static void kafkaProperties(DynamicPropertyRegistry registry) {
+        var index = kafka.getBootstrapServers().lastIndexOf('/');
+        index = index == -1 ? 0 : index + 1;
+        var bootstrapServers = kafka.getBootstrapServers().substring(index);
+        log.warn("\n\nspring.kafka.bootstrap-servers={}\n\n", bootstrapServers);
+        registry.add("spring.kafka.bootstrap-servers", () -> bootstrapServers);
+    }
 
     @Test
     public void testMessageSentMessageReceived() throws InterruptedException {
@@ -27,6 +53,7 @@ public class KafkaTest {
 
         //When
         producer.sendMessage(key, message);
+        Thread.sleep(1000);
 
         //Then
         var consumedMessage = consumer.getQueue().poll(5, TimeUnit.SECONDS);
