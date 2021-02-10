3af6edc8a6eb398c09e9119be5b335aaa0e255ce
Javier Beneito Barquero <malsolo.com@outlook.com>
2021-02-10 16:46:21 +0100

    Testcontainers-boot: create topic before execution in kafka test with testcontainers.

--- src/test/java/com/malsolo/testcontainers/boot/kafka/KafkaTest.java
+++ src/test/java/com/malsolo/testcontainers/boot/kafka/KafkaTest.java
@@ -1,6 +1,13 @@
 package com.malsolo.testcontainers.boot.kafka;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -15,11 +22,6 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SpringBootTest
 @EnableAutoConfiguration(exclude={CassandraReactiveDataAutoConfiguration.class,
     CassandraAutoConfiguration.class, CassandraDataAutoConfiguration.class})
@@ -28,6 +30,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class KafkaTest {
 
     public static final String KAFKA_IMAGE_NAME = "confluentinc/cp-kafka:6.0.1";
+
+    public static final String TOPIC_NAME = "testcontainers-boot-test-topic"; // :|
 
     @Container
     public static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse(KAFKA_IMAGE_NAME));
@@ -45,6 +49,23 @@ public class KafkaTest {
         log.warn("\n\nspring.kafka.bootstrap-servers={}\n\n", bootstrapServers);
         registry.add("spring.kafka.bootstrap-servers", () -> bootstrapServers);
         registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
+    }
+
+    @BeforeAll
+    public static void createTopics() throws IOException, InterruptedException {
+        var createTopicCommand =
+            String.format(
+                "/usr/bin/kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic %s",
+                TOPIC_NAME);
+
+        var execResult = kafka.execInContainer("/bin/sh", "-c", createTopicCommand);
+
+        if (execResult.getExitCode() != 0) {
+            fail();
+        }
+
+        log.warn("Topic {} created: {}", TOPIC_NAME, execResult.getStdout());
+
     }
 
     @Test
